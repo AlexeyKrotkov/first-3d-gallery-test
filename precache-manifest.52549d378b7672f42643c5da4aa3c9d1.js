ea69b82afcef2592cad23031009fe7525c0ee4c7
Aleksei_Krotkov <aleksei_krotkov@epam.com>
2019-06-09 23:16:42 +0400

    add build

---
+++ precache-manifest.52549d378b7672f42643c5da4aa3c9d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252231e77931d51ca525d3fe68c1697d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "790b1165a6e551d599be",
+    "url": "/static/css/main.6a424f64.chunk.css"
+  },
+  {
+    "revision": "b4875259e757a77008c2",
+    "url": "/static/js/2.18e0f6e8.chunk.js"
+  },
+  {
+    "revision": "790b1165a6e551d599be",
+    "url": "/static/js/main.0a5cedf9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e7d5b8a3834ec3f5f87d7fc25ef69240",
+    "url": "/static/media/wall-white.e7d5b8a3.jpg"
+  }
+]);
